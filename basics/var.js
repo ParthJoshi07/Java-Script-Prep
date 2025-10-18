9520826fd6fb1bcdb2d9fc19cc18e5b79b29cb51
ParthJoshi07 <parthsjosh277@gmail.com>
2025-10-18 19:44:09 +0000

    variables

---
+++ basics/var.js
@@ -0,0 +1,13 @@
+const accountId = 14422
+let accountEmail = "parth@google.com"
+var accountPassword = "1234"
+accountCity = "bareilly"
+let accountstate 
+
+// console.log(accountId);
+
+/*
+prefered not to use var due to scope problem
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
