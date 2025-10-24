195e12c12c11dee1f5f7070658f4f300c0878789
ParthJoshi07 <parthsjosh277@gmail.com>
2025-10-24 18:58:59 +0000

    Loops in JS

---
+++ basics/loops.js
@@ -0,0 +1,74 @@
+console.log("Loops in JavaScript")
+
+
+// for (let i =0 ; i<10; i++){
+//     console.log("Java Script")  // Executed i times
+// }
+// console.log("loop has ended")
+
+
+
+
+//Wap to add sum of n numbers :-
+/*let sum = 0
+let n = prompt("Enter the no.")
+for (i = 0 ; i <= n ; i++){
+    sum = sum + i;
+}
+console.log(sum);
+*/
+
+
+
+
+//While lOOP :-
+// i = 0;
+// while (i<10){
+//     console.log(i);
+//     i++
+// }
+
+
+
+
+
+// Do while loop :-
+// i = 0;
+// do{
+//     console.log("Parth Joshi");
+//     i++;
+// } while (i<=10);
+
+
+
+// For-OF loop is JS :-    (We use this loop for Strings and Arrays)
+// str = "Parth Joshi"
+// size = 0;
+// for (i of str){
+//     console.log(i)
+//     size++
+// }
+
+
+// For-in Loop in JS :-  (We us this loop for Objects)
+// const student = {
+//     name : "Pranav",
+//     age : 21,
+//     cgpa : 7.3,
+//     rollno : 44,
+// }
+// for (i in student){
+//     console.log(student[i]); 
+// }
+
+
+
+// Guess the no game :-
+
+// let num  = 24;
+// let n = prompt("guess the no");
+
+// while(num != n){
+//     n = prompt("Wrong, guess the no again");
+// }
+// console.log("No found" , n)
