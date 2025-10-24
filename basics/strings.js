91f91c4a2ef41d003c09844dcd7d264cb76eee13
ParthJoshi07 <parthsjosh277@gmail.com>
2025-10-24 19:33:25 +0000

    strings

---
+++ basics/strings.js
@@ -0,0 +1,31 @@
+console.log("Strings in JS");
+
+// str = "Parth Joshi";
+// console.log(str[0]);  // P
+// console.log(str.length);  // 11 
+
+// str = str.toUpperCase();
+// str = str.toLowerCase();
+// str = str.trim();
+// str = str.slice(0, -3);
+
+
+// console.log(str.replace("joshi","king"))
+
+
+
+
+// Templete Literals (Special String)
+// const obj =  {
+//     item : "Pen",
+//     cost : 10,
+// };
+
+// let opt = `the cost of ${obj.item} is \n${obj.cost} rupees`;
+
+// console.log(opt)
+
+
+
+
+
