a8c276de0e6af1f7c21f31918bee3e81768ffcf2
ParthJoshi07 <parthsjosh277@gmail.com>
2025-10-25 14:45:31 +0000

    functions

---
+++ basics/function.js
@@ -0,0 +1,31 @@
+console.log("Function in JS")
+
+// function add () {
+//     console.log(a+b);
+// }
+// a=1;
+// b=-1;
+// add();
+
+
+
+// function pow (a,b){
+//     return a**b;
+// }
+// console.log(pow(123,3));
+
+
+// Arrow Functions :-  (Modern JS)
+// const div = (a,b) =>{
+//     return a/b;
+// }
+// c = div(2,2);
+
+
+// (For-each) loop in Arrays :-
+// arr = [1,2,3,4,5,6,7,8]
+// arr.forEach (function val(v) {
+//     console.log(v);
+// })
+
+
