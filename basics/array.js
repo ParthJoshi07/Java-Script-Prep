0ead14ffdc206167d9df1c474106cc1b3361215a
ParthJoshi07 <parthsjosh277@gmail.com>
2025-10-25 13:55:41 +0000

    arrays JS

---
+++ basics/array.js
@@ -0,0 +1,42 @@
+console.log("Arrays in JS")
+
+// let a  = [1,2,3,4,5,6,7];
+// a[0] = 9;
+// console.log(a);
+
+
+// using for loop :-
+// n = a.length;
+// for (let i = 0 ; i<n ; i++){
+//     console.log(a[i])
+// }
+
+
+//using for of loop :-
+// for (i of a){
+//     console.log(i);
+// };
+
+
+
+//Wap to find avg of the marks :-
+// marks = [56,32,65,24,67,43];
+// n = marks.length;
+// let sum = 0;
+// for (i of marks){
+//     sum += i;
+// }
+// a = sum/n;
+// console.log( `Avg marks of the student ${a}`);
+
+
+// Array methods :-
+
+// push();  add at the end 
+// pop();   remove form end and return
+// toString();  convert an array to string
+// concat (); join string
+// unshift(); add to start
+// shift(); remove form start
+// slice(); return piece of array
+// splice(); change original array (add,remove,replace)
